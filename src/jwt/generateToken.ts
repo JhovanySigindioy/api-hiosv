52b9d337b04cd9cb50578795e2ca91e028b9f2fa
Wiliam Sigindioy <wiliamjsm1989@gmail.com>
2024-11-28 18:22:19 -0500

    feat/validando reglas

--- src/jwt/generateToken.ts
+++ src/jwt/generateToken.ts
@@ -7,3 +7,12 @@
 //     return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "1h" });
 
 // };
+// import jwt from "jsonwebtoken";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// export const generateToken = (userId: string): string => {
+//     return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+// };
