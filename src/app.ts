ce4383e71f681c52542167e4056ada8fd723365f
Wiliam Sigindioy <wiliamjsm1989@gmail.com>
2024-11-28 15:10:46 -0500

    Actualizacion app.ts se define obtener el puerto que asigne railway

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
 import { serverApp } from "./server/serverApp";
 import dotenv from "dotenv";
-import { serviceLogin } from "./services/serviceLogin";
 
 dotenv.config();
 
-const portServer: string | number = process.env.SERVER_EXPRESS_PORT || 5000;
+const portServer: string | number = process.env.PORT || 5000;
 
 serverApp.listen(portServer, (): void => {
     console.log(`Servidor express corriendo satisfactoriamente en puerto ${portServer}`);
